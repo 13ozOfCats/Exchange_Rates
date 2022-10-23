bb27a2696981e0757218e5f1c09a1191f7ce716c
Помазкин Никита <nikpomazkin@gmail.com>
2022-10-24 02:57:19 +0300

    added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:13ozOfCats/Exchange_Rates.git master:gh-pages
+cd -
