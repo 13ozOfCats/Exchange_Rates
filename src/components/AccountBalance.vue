ac8f05d05d3460d220eeabec6283f7b91883df54
Помазкин Никита <nikpomazkin@gmail.com>
2022-10-23 03:07:15 +0300

    added AccountBalance component

---
+++ src/components/AccountBalance.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "AccountBalance",
+  props: {
+    wallet: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="account-balance">
+    <div class="title">
+      Баланс
+    </div>
+    <table class="table">
+      <tbody class="table__tbody">
+        <tr v-for="(value, key) in wallet" :key="key" class="table__row">
+          <td class="table__rate">
+            {{ key }}
+          </td>
+          <td class="table__val">
+            {{ value }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
